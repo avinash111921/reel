312dad441fdd09f81b7f5b49b384609e6928313f
Aviansh verma <avinash1921avi@gmail.com>
2025-02-02 11:46:41 +0530

    models of video and user

--- lib/db.ts
+++ lib/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI!; //! -> 100% yahe milega 
 
 if(!MONGODB_URI){
-    throw new Error("Please Define monoDb uri in ENV File");
+    throw new Error("Please Define MongoDB_URI in ENV File");
 }
 
 //global -> variable 
@@ -14,13 +14,18 @@ if(!cached){
 }
 
 export async function connectToDatabase(){
+
     if(cached.conn){
         return cached.conn
     }
+
     if(!cached.promise){
         const option = {
-            bufferCommands : true, //?
-            maxPooSize : 10 //kitna connection ho skta hai mongoDb sai ekBaar mai
+            bufferCommands : true, 
+            //This option enables buffering of Mongoose commands when the connection to MongoDB is not yet established.
+            // If true, operations like .find() or .save() will be queued until the connection is ready.
+            // If false, commands will fail immediately if the connection is not ready.
+            maxPoolSize : 10 // Defines the maximum number of connections in the pool
         }
         cached.promise = mongoose
         .connect(MONGODB_URI,option)
--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,11 @@
 import { Connection } from "mongoose"
 
 declare global {
+    //You are extending the global scope (global) with a mongoose object.
     var mongoose : {
-        conn : Connection | null
-        promise : Promise<Connection> | null
+        //wo props
+        conn : Connection | null //Holds the active Mongoose connection or null if not connected.
+        promise : Promise<Connection> | null  //Stores the promise resolving to a Mongoose connection or null if not yet initialized.
     };
 }
 
