0fdccb1ce3c38e6004d32e5e231a2ddee074f962
Aviansh verma <avinash1921avi@gmail.com>
2025-02-02 22:11:51 +0530

    nothing

--- types.d.ts
+++ types.d.ts
@@ -3,7 +3,7 @@ import { Connection } from "mongoose"
 declare global {
     //You are extending the global scope (global) with a mongoose object.
     var mongoose : {
-        //wo props
+        //properties
         conn : Connection | null //Holds the active Mongoose connection or null if not connected.
         promise : Promise<Connection> | null  //Stores the promise resolving to a Mongoose connection or null if not yet initialized.
     };
